d04c6e61597c155d822dc2f2fb8951677277b648
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-07-22 11:07:23 +0530

    First Commit-sanjay

--- server/adminController/adminController.js
+++ server/adminController/adminController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/Category");
+//get Data
 exports.home=async(req,res)=>{
     try {
         const data=await Category.find({})
@@ -26,6 +27,68 @@ exports.addData = async (req, res) => {
       });
       await newData.save();
       return res.status(200).json({ message: "Data is Inserted Succesfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  };
+
+  //get mem with id
+  exports.getmem = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const wholeData = await Category.findById(id);
+      if (!wholeData) {
+        return res.status(404).json({
+          message: "Error in Fectching Data",
+        });
+      }
+      return res.status(200).json(wholeData);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  };
+  //update data
+ exports.updateData = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const { name } = req.body;
+      if (!id) {
+        return res.status(400).json({
+          message: "id is missing",
+        });
+      }
+      const upDatat = await Category.findByIdAndUpdate(id, {
+       name
+      });
+      if (!upDatat) {
+        return res.status(404).json({ message: "Error in Data updateing" });
+      }
+      return res.status(200).json({ message: "Data is Updated", upDatat });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  };
+
+  //delete data
+  exports.deleteData = async (req, res) => {
+    try {
+      const id = req.params.id;
+      if (!id) {
+        return res.status(400).json({
+          message: "id is missing",
+        });
+      }
+      const fetchId=await Category.findById(id);
+      if(!fetchId){
+        return res.status(404).json({ message: "Not found the data with Id" });
+      }
+      const upDatat = await Category.findByIdAndDelete(id);
+      if (upDatat) {
+        return res.status(200).json({ message: "Data is Deleted" });
+      }
     } catch (error) {
       console.log(error);
       return res.status(400).json(error);
--- server/adminRoutes/adminRoutes.js
+++ server/adminRoutes/adminRoutes.js
@@ -2,5 +2,8 @@ const express=require("express");
 const router=express.Router();
 const adminController=require('../adminController/adminController');
 router.get('/home',adminController.home);
-router.post('/add',adminController.addData)
+router.post('/add',adminController.addData);
+router.get('/getmem/:id',adminController.getmem);
+router.put("/update/:id", adminController.updateData);
+router.delete("/delete/:id",adminController.deleteData);
 module.exports=router;
