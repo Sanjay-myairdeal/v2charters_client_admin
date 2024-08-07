3685e0b372dc360e2e0caec31a0e0afc99d1913e
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-08-07 16:54:16 +0530

    sub category commity

--- server/adminController/categorymodifyController.js
+++ server/adminController/categorymodifyController.js
@@ -1,7 +1,9 @@
 const Categorymodify = require("../models/Categorymodify");
 const Subcategory = require("../models/Subcategory");
 const cloudinary = require("cloudinary").v2;
-// Cloudinary configuration
+/**
+ *  Cloudinary configuration
+ */
 cloudinary.config({
   cloud_name: "dybrajkta",
   api_key: "921983243972892",
@@ -29,7 +31,7 @@ exports.getModifyCategories = async (req, res) => {
 exports.addModifyCategories = async (req, res) => {
   try {
     const { chartertype, description } = req.body;
-    console.log(chartertype, description);
+    // console.log(chartertype, description);
     // Validate required fields
     if (!chartertype || !description) {
       return res.status(400).json({ message: "Missing fields" });
@@ -56,6 +58,72 @@ exports.addModifyCategories = async (req, res) => {
     return res.status(200).json({ message: "Data inserted successfully" });
   } catch (error) {
     console.error("Error adding category:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+/**
+ * Edit Category by id
+ */
+exports.editModifyCharterById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { chartertype, description } = req.body;
+
+    if (!id || (!chartertype && !description && !req.file)) {
+      return res
+        .status(400)
+        .json({ message: "ID or fields to update are missing" });
+    }
+
+    let image;
+
+    if (req.file) {
+      // Upload the new image to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      image = result.secure_url;
+    } else {
+      image = req.body.image;
+    }
+
+    const updatedCategory = await Categorymodify.findByIdAndUpdate(
+      id,
+      { chartertype, description, image },
+      { new: true }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ message: "Error in updating data" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Data updated successfully", updatedCategory });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+/**
+ * Delete Category Data
+ */
+exports.deleteModifyCharterById = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      return res.status(400).json({ message: "ID is missing" });
+    }
+
+    const category = await Categorymodify.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: "Data not found" });
+    }
+
+    await Categorymodify.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Data deleted successfully" });
+  } catch (error) {
+    console.error(error);
     return res.status(500).json({ message: "Server error" });
   }
 };
@@ -154,24 +222,130 @@ exports.addSubCategories = async (req, res) => {
 exports.getsubCategorybyType = async (req, res) => {
   try {
     const type = req.params.chartertype;
-    console.log(type)
+    console.log(type);
     if (!type) {
-      return res
-        .status(404)
-        .json({ message: "Charter type is missing" });
+      return res.status(404).json({ message: "Charter type is missing" });
     }
-    const filldata = await Subcategory.find({ chartertype:type });
+    const filldata = await Subcategory.find({ chartertype: type });
     if (filldata.length === 0) {
-      return res.status(400).json({ message: "No such subcategory exists for the given charter type" });
+      return res
+        .status(400)
+        .json({
+          message: "No such subcategory exists for the given charter type",
+        });
     }
-    return res
-      .status(200)
-      .json({
-        message: "Flights are fetched successfully",
-        sortedData: filldata,
-      });
+    return res.status(200).json({
+      message: "Flights are fetched successfully",
+      sortedData: filldata,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+/**
+ * Edit sub category by Id
+ */
+
+exports.editSubCategoryById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      return res.status(404).json({ message: "Id is missing" });
+    }
+    const {
+      chartertype,
+      pax,
+      speed,
+      price,
+      description,
+      availability,
+      bookingtype,
+      departure,
+      arrival,
+      journeytype,
+      date,
+    } = req.body;
+    if (
+      !chartertype ||
+      !pax ||
+      !speed ||
+      !price ||
+      !availability ||
+      !description ||
+      !bookingtype ||
+      !departure ||
+      !arrival ||
+      !journeytype ||
+      !date
+    ) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+
+    let image;
+
+    if (req.file) {
+      // Upload the new image to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      image = result.secure_url;
+    } else {
+      image = req.body.image;
+    }
+
+    const updatedData = await Subcategory.findByIdAndUpdate(
+      id,
+      {
+        chartertype,
+        pax,
+        speed,
+        price,
+        description,
+        availability,
+        bookingtype,
+        departure,
+        arrival,
+        journeytype,
+        date,
+        image: image,
+      },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(400).json({ message: "Error in updating the data" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Data updated successfully", updated: updatedData });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+/**
+ * Delete Sub category
+ */
+
+exports.deleteModifySubCharterById = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      return res.status(400).json({ message: "ID is missing" });
+    }
+
+    const category = await Subcategory.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: "Data not found" });
+    }
+
+    await Subcategory.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Data deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
   }
 };
--- server/adminRoutes/adminRoutes.js
+++ server/adminRoutes/adminRoutes.js
@@ -1,110 +1,143 @@
-const express=require("express");
-const router=express.Router();
-const multer  = require('multer')
-const adminController=require('../adminController/adminController');
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const adminController = require("../adminController/adminController");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
-const loginController=require('../adminController/loginController')
-const modifyController=require('../adminController/categorymodifyController')
+const loginController = require("../adminController/loginController");
+const modifyController = require("../adminController/categorymodifyController");
 
 // Cloudinary configuration
 cloudinary.config({
-    cloud_name: "dybrajkta",
-    api_key: "921983243972892",
-    api_secret: "c4n72FykTGrxsKpDzpADvNsqf5U",
-  });
+  cloud_name: "dybrajkta",
+  api_key: "921983243972892",
+  api_secret: "c4n72FykTGrxsKpDzpADvNsqf5U",
+});
 // Multer configuration
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'uploads', 
-      format: async (req, file) => {
-        const supportedFormats = ['jpg', 'jpeg', 'png', 'gif'];
-        const fileFormat = file.mimetype.split('/')[1];
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads",
+    format: async (req, file) => {
+      const supportedFormats = ["jpg", "jpeg", "png", "gif"];
+      const fileFormat = file.mimetype.split("/")[1];
 
-        return supportedFormats.includes(fileFormat) ? fileFormat : 'jpg';
-      },
-      public_id: (req, file) => Date.now() + '-' + file.originalname,
+      return supportedFormats.includes(fileFormat) ? fileFormat : "jpg";
     },
-  });
+    public_id: (req, file) => Date.now() + "-" + file.originalname,
+  },
+});
 
-
-
-const upload = multer({ 
-    storage: storage,
-    
+const upload = multer({
+  storage: storage,
 });
 /** Category Section Routes Starts */
 
-
-router.get('/getallcategories',adminController.getAllCategories);
-router.post('/addchartercategory',upload.single('image'),adminController.addCharterCategory);
-router.get('/getcharterbyid/:id',adminController.getCharterById);
-router.put("/editcharterbyid/:id",upload.single('image'), adminController.editCharterById);
-router.delete("/deletecharterbyid/:id",adminController.deleteCharterById);
-
-
+router.get("/getallcategories", adminController.getAllCategories);
+router.post(
+  "/addchartercategory",
+  upload.single("image"),
+  adminController.addCharterCategory
+);
+router.get("/getcharterbyid/:id", adminController.getCharterById);
+router.put(
+  "/editcharterbyid/:id",
+  upload.single("image"),
+  adminController.editCharterById
+);
+router.delete("/deletecharterbyid/:id", adminController.deleteCharterById);
 
 /** Booking Section Routes Starts */
-router.get('/getallbookings',adminController.getAllBookings);
-router.post('/addbooking',adminController.addBooking);
-router.post('/sorted',adminController.filterDate);
-router.delete('/deletebookingbyid/:id',adminController.deleteBookingById);
-
-
-
+router.get("/getallbookings", adminController.getAllBookings);
+router.post("/addbooking", adminController.addBooking);
+router.post("/sorted", adminController.filterDate);
+router.delete("/deletebookingbyid/:id", adminController.deleteBookingById);
 
 /** Empty legs Section Starts */
-router.get('/getallemptylegs',adminController.getAllEmptyLegs);
-router.post('/addemptylegs',upload.single('image'),adminController.addEmptyLegs);
+router.get("/getallemptylegs", adminController.getAllEmptyLegs);
+router.post(
+  "/addemptylegs",
+  upload.single("image"),
+  adminController.addEmptyLegs
+);
 router.get("/getemptylegsbyid/:id", adminController.getEmptyLegById);
-router.put('/editemptylegsbyid/:id',upload.single('image'),adminController.editEmptyLegsById);
-router.delete('/deleteemptylegsbyid/:id',adminController.deleteEmptyLegsById);
-
+router.put(
+  "/editemptylegsbyid/:id",
+  upload.single("image"),
+  adminController.editEmptyLegsById
+);
+router.delete("/deleteemptylegsbyid/:id", adminController.deleteEmptyLegsById);
 
 /** Empty Leg Booking Routes */
 
-
-router.get('/getallemptylegbookings',adminController.getAllEmptyBookings);
-router.post('/addemptylegbooking',adminController.addEmptyLegBooking);
-router.get("/getemptylegbookingbyid/:id", adminController.getEmptylegBookingById);
-router.post('/filteremptylegbooking',adminController.filterEmptyLegDate);
-router.delete('/deleteemptylegbookingbyid/:id',adminController.deleteEmptyLegBookingById);
+router.get("/getallemptylegbookings", adminController.getAllEmptyBookings);
+router.post("/addemptylegbooking", adminController.addEmptyLegBooking);
+router.get(
+  "/getemptylegbookingbyid/:id",
+  adminController.getEmptylegBookingById
+);
+router.post("/filteremptylegbooking", adminController.filterEmptyLegDate);
+router.delete(
+  "/deleteemptylegbookingbyid/:id",
+  adminController.deleteEmptyLegBookingById
+);
 
 /**Register and Login Routes */
-router.post('/register',loginController.register);
-router.post('/login',loginController.login);
-router.delete('/deleteadmin/:id',loginController.deleteAdmin);
-
+router.post("/register", loginController.register);
+router.post("/login", loginController.login);
+router.delete("/deleteadmin/:id", loginController.deleteAdmin);
 
 /** Feedback Section */
-router.post('/addfeedback',adminController.addFeedback);
-router.get('/getallfeedback',adminController.getAllFeedbacks)
-router.delete('/deletefeedbackbyid/:id',adminController.deleteFeedbackById)
-module.exports=router;
+router.post("/addfeedback", adminController.addFeedback);
+router.get("/getallfeedback", adminController.getAllFeedbacks);
+router.delete("/deletefeedbackbyid/:id", adminController.deleteFeedbackById);
+module.exports = router;
 
 /**Search Route */
-router.post('/search',adminController.Search);
-
+router.post("/search", adminController.Search);
 
 /**Sub Categories */
-router.get('/categories',adminController.explorecategories);
-router.get('/categories/:id',adminController.exploreCategoriesById);
+router.get("/categories", adminController.explorecategories);
+router.get("/categories/:id", adminController.exploreCategoriesById);
 
 /**
  * Routes of Modify Schema
  */
-router.get('/modifycategory',modifyController.getModifyCategories);
-router.post('/addmodifycategory',upload.single('image'),modifyController.addModifyCategories)
-
+router.get("/modifycategory", modifyController.getModifyCategories);
+router.post(
+  "/addmodifycategory",
+  upload.single("image"),
+  modifyController.addModifyCategories
+);
+router.put(
+  "/editmodifycharterbyid/:id",
+  upload.single("image"),
+  modifyController.editModifyCharterById
+);
+router.delete(
+  "/deletemodifycharterbyid/:id",
+  modifyController.deleteModifyCharterById
+);
 /**
  * Sub Categories Routes
  */
 
-router.get('/getallsubcategories',modifyController.getSubCategories);
-router.post('/addsubcategory',upload.single('image'),modifyController.addSubCategories);
-
+router.get("/getallsubcategories", modifyController.getSubCategories);
+router.post(
+  "/addsubcategory",
+  upload.single("image"),
+  modifyController.addSubCategories
+);
+router.put(
+  "/editmodifysubcharterbyid/:id",
+  upload.single("image"),
+  modifyController.editSubCategoryById
+);
+router.delete(
+  "/deletemodifysubcharterbyid/:id",
+  modifyController.deleteModifySubCharterById
+);
 /**
  * Filter Category Data
  */
-router.get('/filter/:chartertype',modifyController.getsubCategorybyType)
+router.get("/filter/:chartertype", modifyController.getsubCategorybyType);
